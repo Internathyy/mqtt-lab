eab06040b260d813ef7932b295bdc5f799c0ba07
Nathaniel Ainsworth <239359880+Internathyy@users.noreply.github.com>
2026-05-26 08:57:42 +0000

    d

--- test_publisher.c
+++ test_publisher.c
@@ -41,91 +41,132 @@ int serialize_to_json(const SensorReading* r, char* buf, size_t buf_len){
         return -1;
     }
 
-    snprintf("seq: %i \n station_id: %s \n timestamp: %s temperature_c: %f \n humidity_pct: %f \n", seq, station_id, timestamp, temperature_c, humidity_pct);
-}
-
-/* Generate a simple JSON payload with dummy sensor data */
-static void build_payload(char *buf, size_t len, int seq) {
-    time_t now = time(NULL);
-    struct tm *t = gmtime(&now);
-    char ts[30];
-    strftime(ts, sizeof(ts), "%Y-%m-%dT%H:%M:%SZ", t);
-
-    /* Simple simulated values */
-    float temp     = 18.0f + (float)(rand() % 100) / 20.0f;   /* 18.0 – 23.0 */
-    float humidity = 50.0f + (float)(rand() % 300) / 10.0f;   /* 50.0 – 80.0 */
-
-    snprintf(buf, len,
-        "{\"seq\":%d,\"station_id\":\"S1\","
-        "\"timestamp\":\"%s\","
-        "\"temperature_c\":%.1f,"
-        "\"humidity_pct\":%.1f}",
-        seq, ts, temp, humidity);
-}
-
-/* Callback: called when connection is established */
-static void on_connect(struct mosquitto *mosq, void *userdata, int rc) {
-    if (rc == 0) {
-        printf("[publisher] Connected to %s:%d\n", BROKER, PORT);
-    } else {
-        fprintf(stderr, "[publisher] Connection failed: %s\n",
-                mosquitto_connack_string(rc));
-        mosquitto_disconnect(mosq);
-    }
-}
-
-/* Callback: called after each message is published */
-static void on_publish(struct mosquitto *mosq, void *userdata, int mid) {
-    printf("[publisher] Message %d delivered to broker\n", mid);
-}
-
-int main(void) {
-    srand((unsigned int)time(NULL));
-
-    mosquitto_lib_init();
-
-    struct mosquitto *mosq = mosquitto_new("mqtt-lab-publisher", true, NULL);
-    if (!mosq) {
-        fprintf(stderr, "[publisher] Failed to create mosquitto instance\n");
-        mosquitto_lib_cleanup();
-        return 1;
+    if (r == NULL){
+        printf("Error: SensorReading is empty \n ");
+        return -1;
     }
 
-    mosquitto_connect_callback_set(mosq, on_connect);
-    mosquitto_publish_callback_set(mosq, on_publish);
+    int written = snprintf(buf, buf_len, "{\"seq\":%i,\"station_id\":\"%s\",\"timestamp\":\"%s\",\"temperature_c\":%.1f,\"humidity_pct\":%.1f}", r->seq, r->station_id, r->timestamp, r->temperature_c, r->humidity_pct);
 
-    int rc = mosquitto_connect(mosq, BROKER, PORT, 60);
-    if (rc != MOSQ_ERR_SUCCESS) {
-        fprintf(stderr, "[publisher] Could not connect: %s\n",
-                mosquitto_strerror(rc));
-        mosquitto_destroy(mosq);
-        mosquitto_lib_cleanup();
-        return 1;
+    if (written < 0){
+        printf("Fehler beim befüllen vom Buffer");
+        return -1;
     }
 
-    /* Start the network loop in background thread */
-    mosquitto_loop_start(mosq);
-
-    char payload[256];
-    printf("[publisher] Sending %d messages to topic: %s\n\n", MSG_COUNT, TOPIC);
-
-    for (int i = 1; i <= MSG_COUNT; i++) {
-        build_payload(payload, sizeof(payload), i);
-        printf("[publisher] Publishing: %s\n", payload);
-
-        rc = mosquitto_publish(mosq, NULL, TOPIC,
-                               (int)strlen(payload), payload, 1, false);
-        if (rc != MOSQ_ERR_SUCCESS) {
-            fprintf(stderr, "[publisher] Publish error: %s\n",
-                    mosquitto_strerror(rc));
-        }
-        sleep(INTERVAL_S);
+    if ((size_t)written >= buf_len){
+        printf("Buffer zu klein, Inhalt abgeschnitten");
+        return -1;
     }
 
-    printf("\n[publisher] Done. Disconnecting.\n");
-    mosquitto_disconnect(mosq);
-    mosquitto_loop_stop(mosq, true);
-    mosquitto_destroy(mosq);
-    mosquitto_lib_cleanup();
+    return 0;
+
+}
+
+int main(void){
+
+    //sensorreading mock
+
+    SensorReading reading = {
+    1,
+    "S1",
+    "2026-05-26T10:53:21Z",
+    17.8,
+    30.5,
+    };
+
+    char json[256];
+
+    serialize_to_json(&reading, json, sizeof(json));
+
+
     return 0;
 }
+
+// /* Generate a simple JSON payload with dummy sensor data */
+// static void build_payload(char *buf, size_t len, int seq) {
+//     time_t now = time(NULL);
+//     struct tm *t = gmtime(&now);
+//     char ts[30];
+//     strftime(ts, sizeof(ts), "%Y-%m-%dT%H:%M:%SZ", t);
+
+//     /* Simple simulated values */
+//     float temp     = 18.0f + (float)(rand() % 100) / 20.0f;   /* 18.0 – 23.0 */
+//     float humidity = 50.0f + (float)(rand() % 300) / 10.0f;   /* 50.0 – 80.0 */
+
+//     snprintf(buf, len,
+//         "{\"seq\":%d,\"station_id\":\"S1\","
+//         "\"timestamp\":\"%s\","
+//         "\"temperature_c\":%.1f,"
+//         "\"humidity_pct\":%.1f}",
+//         seq, ts, temp, humidity);
+// }
+
+// /* Callback: called when connection is established */
+// static void on_connect(struct mosquitto *mosq, void *userdata, int rc) {
+//     if (rc == 0) {
+//         printf("[publisher] Connected to %s:%d\n", BROKER, PORT);
+//     } else {
+//         fprintf(stderr, "[publisher] Connection failed: %s\n",
+//                 mosquitto_connack_string(rc));
+//         mosquitto_disconnect(mosq);
+//     }
+// }
+
+// /* Callback: called after each message is published */
+// static void on_publish(struct mosquitto *mosq, void *userdata, int mid) {
+//     printf("[publisher] Message %d delivered to broker\n", mid);
+// }
+
+// int main(void) {
+//     srand((unsigned int)time(NULL));
+
+//     mosquitto_lib_init();
+
+//     struct mosquitto *mosq = mosquitto_new("mqtt-lab-publisher", true, NULL);
+//     if (!mosq) {
+//         fprintf(stderr, "[publisher] Failed to create mosquitto instance\n");
+//         mosquitto_lib_cleanup();
+//         return 1;
+//     }
+
+//     mosquitto_connect_callback_set(mosq, on_connect);
+//     mosquitto_publish_callback_set(mosq, on_publish);
+
+//     int rc = mosquitto_connect(mosq, BROKER, PORT, 60);
+//     if (rc != MOSQ_ERR_SUCCESS) {
+//         fprintf(stderr, "[publisher] Could not connect: %s\n",
+//                 mosquitto_strerror(rc));
+//         mosquitto_destroy(mosq);
+//         mosquitto_lib_cleanup();
+//         return 1;
+//     }
+
+//     /* Start the network loop in background thread */
+//     mosquitto_loop_start(mosq);
+
+//     char payload[256];
+//     printf("[publisher] Sending %d messages to topic: %s\n\n", MSG_COUNT, TOPIC);
+
+//     for (int i = 1; i <= MSG_COUNT; i++) {
+//         build_payload(payload, sizeof(payload), i);
+//         printf("[publisher] Publishing: %s\n", payload);
+
+//         rc = mosquitto_publish(mosq, NULL, TOPIC,
+//                                (int)strlen(payload), payload, 1, false);
+//         if (rc != MOSQ_ERR_SUCCESS) {
+//             fprintf(stderr, "[publisher] Publish error: %s\n",
+//                     mosquitto_strerror(rc));
+//         }
+//         sleep(INTERVAL_S);
+//     }
+
+//     printf("\n[publisher] Done. Disconnecting.\n");
+//     mosquitto_disconnect(mosq);
+//     mosquitto_loop_stop(mosq, true);
+//     mosquitto_destroy(mosq);
+//     mosquitto_lib_cleanup();
+//     return 0;
+// }
+
+
+
